5359278b55b914a396a12abe0a690de79091f5b7
Codaishin <codaishin@gmail.com>
2024-03-25 13:56:39 +0100

    feat: add page tests

---
+++ src/app/page.test.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from './page'
+
+describe('Page', () => {
+	it('renders message', () => {
+		render(<Page/>)
+
+		const header = screen.getByRole('heading', { level: 1 })
+
+		expect(header).toHaveTextContent('This is my site!! Be amazed!!')
+	})
+})
