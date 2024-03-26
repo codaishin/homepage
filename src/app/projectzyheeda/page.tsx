7edc3692b11d92c42c4c68aee65b3536c312df7b
Codaishin <codaishin@gmail.com>
2024-03-26 17:00:25 +0100

    feat: add some pages

---
+++ src/app/projectzyheeda/page.tsx
@@ -0,0 +1,11 @@
+import { Navigation } from '../../components/nav'
+import { Page } from '@/components/page'
+
+const ProjectZyheeda = () =>
+	<Page path='Project Zyheeda' navigation={ Navigation }>
+		<h1>
+			Project Zyheeda
+		</h1>
+	</Page>
+
+export default ProjectZyheeda
